f2ab1db8ec8fdd7c402d3b92c81e51df59fe6e37
Jaime Trocha <jtrocha04@gmail.com>
2022-10-17 20:36:21 -0500

    Instalacion de Bootstrap y conexion con Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYub148GAha4N_L0lEGLG9nRZg4890B8E",
+  authDomain: "crud-app-fcadf.firebaseapp.com",
+  projectId: "crud-app-fcadf",
+  storageBucket: "crud-app-fcadf.appspot.com",
+  messagingSenderId: "1061219166245",
+  appId: "1:1061219166245:web:7b913fa10cf521d25777cc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
